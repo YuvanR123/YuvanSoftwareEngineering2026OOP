3dfe0a54da0b58fd231ef9b38d62ac2f8d6fc89d
YuvanR123 <yuvan.rajesh@education.nsw.gov.au>
2026-05-18 00:57:23 +0000

    commit changes to 4c and 5a

--- OOP /5a._fighter_class_activities.py
+++ OOP /5a._fighter_class_activities.py
@@ -6,3 +6,28 @@
 import random
 
 class Fighter:
+ def __init__(self, name, starting_health, weapon, shield):
+    self.name = name
+    self.health = starting_health
+    self.weapon = weapon
+    self.shield = shield
+
+ def random_attack(self):
+   attack_power = random.randint(self.weapon*2, self.weapon/2)
+   print('Attack Power: ' + attack_power)
+   return attack_power
+
+ def report(self):
+   print(self.name + 'Health: ' + str(self.health))
+
+
+   
+fighter1 = Fighter('Jack', 100, 90, 50)
+fighter2 = Fighter('Monkey', 200, 60, 120)
+fighter1.report()
+fighter2.report()
+print('Fighter 1 attacks Fighter 2!')
+fighter2.health -= fighter1.random_attack()
+fighter2.report()
+
+

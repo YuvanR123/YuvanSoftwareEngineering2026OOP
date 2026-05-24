6d33fbb8f1d7f3dd1035dd0fcf43e4b07a40a8a8
YuvanR123 <yuvan.rajesh@education.nsw.gov.au>
2026-05-24 23:40:06 +0000

    commit changes to oop_game

--- OOP /oop_game.py
+++ OOP /oop_game.py
@@ -68,9 +68,10 @@ class Wizard(Fighter): #Inheritance class called Wizard from the Fighter Class
        
 
 class Ninja(Fighter): #Inheritence class called Ninja from Fighter Class
-    def __init__(self,name,starting_health,weapon,shield,dodge):
+    def __init__(self,name,starting_health,weapon,shield,dodge,ninja_star):
         super().__init__(name,starting_health,weapon,shield)
         self.dodge = dodge
+        self.ninja_star = ninja_star
     
     def dodge_attack(self,attack_power):
         dodge_chance = random.randint(1,100)
@@ -80,8 +81,52 @@ class Ninja(Fighter): #Inheritence class called Ninja from Fighter Class
             print(self.name + 'failed to dodge')
             self.defend(attack_power)
 
+    def star(self,ninja_star):
+        star_chance = random.randint(1,50)
+        if star_chance <= self.ninja_star:
+         
+         print('Jake uses his ninja star!')
+        else:
+         print("Jake's ninja star missed!")
+        return ninja_star
+        
+
+class Dragon:
+    def __init__(self,name,starting_health,shield,beam):
+     self.name = name
+     self.__health = starting_health
+     self.shield = shield
+     self.beam = beam
+
+    def report(self): #Reports Stats
+        print(self.name+':'+ ' Health: '+ str(self.__health))
+
+    def is_dead(self): #Checks whether character is alive or dead
+        if self.__health <= 0:
+            return True
+        else:
+            return False
+    
+    def beam_attack(self):
+     beam_power = random.randint(self.beam//2,self.beam*2)
+     print('Beam Power: ' + beam_power)
+     return beam_power
+
+    def beam_buff(self):
+        beam_power_buff = self.beam*2
+        return beam_power_buff
+    
+    def defend(self,attack_power):
+        damage = attack_power - self.shield
+        if damage >  0:
+            self.__health -= damage
+            print('Damage:', damage)
+        else:
+            print('No damage')
+
+    
 jake = Fighter('Jake',100,60,20)
 wizard = Wizard('The Grey Wizard',160,30,10,50)
-ninja = Ninja('Jake The Ninja', 90, 70, 60, 35)
-
+ninja = Ninja('Jake The Ninja', 90, 70, 60, 35,28)
+dragon = Dragon('Nocturnal Dragon', 200,50,40)
 

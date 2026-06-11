14ee79ee05f14e9efe7b821895096ff3ef010b03
YuvanR123 <yuvan.rajesh@education.nsw.gov.au>
2026-06-11 03:00:22 +0000

    commit everything

--- OOP /Instantiation_encapsulation_polymorphism.py
+++ OOP /Instantiation_encapsulation_polymorphism.py
@@ -1,1 +1,21 @@
-...
+#---begin Python ---
+class Car:
+    def __init__(self, make, model, year, colour):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.colour = colour
+    
+    def start(self):
+        print(f"{self.make} {self.model} is starting.")
+
+    def stop(self):
+        print(f"{self.make} {self.model} is stopping.")
+
+# Instantiating objects from the Car class
+car1 = Car("Toyota", "Camry", 2020, "Red")
+car2 = Car("Honda", "Civic", 2018, "Blue")
+
+car1.start()  # Output: Toyota Camry is starting.
+car2.stop()   # Output: Honda Civic is stopping.
+#--- end python ---
